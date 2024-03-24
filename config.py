e7a141f8f40a6ae9f75f464b1f23850848fe0dc5
IamMr7 <163332665+IamMr7@users.noreply.github.com>
2024-03-24 11:33:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ START_IMG = getenv("START_IMG", "https://graph.org/file/19ca5a3150d7b2416d3c1.jp
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/D_Z_A_R")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/A_S_A_33")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/D_Z_A_R")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6456641797").split()))
